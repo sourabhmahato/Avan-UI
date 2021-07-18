663303ff1c5dfa97227f53e35eeee2d8b23da075
Sourabh Mahato <54588817+sourabhmahato@users.noreply.github.com>
2021-07-18 13:49:25 +0530

    refactor: file names

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -1,5 +1,5 @@
 import { NavLink } from "react-router-dom";
-import styles from "./SideBar.module.css";
+import styles from "./Sidebar.module.css";
 import { useRef } from "react";
 import { useOnClickOutside } from "./useOnClickOutside";
 
